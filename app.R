f6bcc5cdd74c0bf5e9213dd7c1e23cf9ed5899cb
Ivory <IvoryEC@gmail.com>
2021-02-21 19:53:20 -0500

    Make app.R a file that can give users the optoins to launch with app using the 'Run App' button in RStudio.

--- app.R
+++ app.R
@@ -1,3 +1,16 @@
 
-pkgload::load_all(".")
-shiny_biolockj()
+## This is an alternative to the primary launch mechanism:
+# devtools::install_github(IvoryC/shinyBio)
+# library("shinyBioLockJ")
+# shiny_biolockj()
+
+# This was a haphazardly put together by loosely following instructions to make 
+# this shiny app package compatible with remote hosting options like shiny.io
+
+# pkgload::load_all(".")
+for (file in dir("R)")) source(file)
+
+# shiny_biolockj()
+ui = biolockj_ui()
+server <- biolockj_server(input, output, session)
+shinyApp(ui, server)
