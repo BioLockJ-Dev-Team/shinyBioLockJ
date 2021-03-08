d2e22ca628e07c5f5bf3212a58ad81b60a72d356
Ivory <IvoryEC@gmail.com>
2021-03-08 01:02:44 -0500

    updates in man pages.

--- R/is_key_module_prop.R
+++ R/is_key_module_prop.R
@@ -1,3 +1,14 @@
+#' Is this a key property
+#' 
+#' The logic for this mimics the logic used to separate properties in the user guide.
+#' A property is a "key" property for a given module if the property is not also a general property OR
+#' if the description is different from the general property description.
+#'
+#' @param prop 
+#' @param genPropInfo 
+#'
+#' @return boolean
+#'
 is_key_module_prop <- function(prop, genPropInfo=BioLockR::propInfo()){
     propName = prop$property
     if ( propName %in% names(genPropInfo)){
